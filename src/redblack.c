0138339bcb081f32c9cfa3ee4daa85648e619900
crabbygrindall90@gmail.com <crabbygrindall90@gmail.com>
2017-01-25 22:02:57 -0500

    Rb trees insertion

--- src/redblack.c
+++ src/redblack.c
@@ -26,18 +26,74 @@ rb_node * rb_node_init()
 void rb_insert(rb_node * root, char * string)
 {
 	int v = lex_less(root->string, string);
+	rb_node * child = NULL;
+	int right = 1;
 	if (v >= 0) {
-		if (root->right)
+		if (root->right) {
 			rb_insert(root->right, string);
-		else
+			return;
+		}
+		else {
 			root->right = rb_new(string);
+			root->right->parent = root;
+			child = root->right;
+		}
 	}
 	else {
-		if (root->left)
+		if (root->left) {
 			rb_insert(root->left, string);
-		else
+			return;
+		}
+		else {
 			root->left = rb_new(string);
+			root->left->parent = root;
+			child = root->left;
+			right = 0;
+		}
 	}
+	if (root->rb)
+	{
+		if (right && (!root->left || (root->left && !root->left->rb)))
+		{
+			//rotate left
+			rb_rotate_left(child);
+		}
+		else if (!right && (!root->right || (root->right && !root->right->rb)))
+		{
+			//rotate right
+			rb_rotate_right(child);
+		}
+		else if (right && (!root->left || (root->left && root->left->rb)))
+		{
+			child->rb = 0;
+			root->left->rb = 0;
+			if (root->parent->parent != NULL) {
+				root->parent->rb = 1;
+			}
+		}
+		else if (!right && (!root->right || (root->right && root->right->rb)))
+		{
+			child->rb = 0;
+			root->right->rb = 0;
+			if (root->parent->parent != NULL) {
+				root->parent->rb = 1;
+			}
+		}
+	}
+}
+
+void rb_rotate_right(rb_node * node)
+{
+	rb_node * p = node->parent;
+	p->left = node->right;
+	node->rb = p;
+}
+
+void rb_rotate_left(rb_node * node)
+{
+	rb_node * p = node->parent;
+	p->right = node->left;
+	node->rb = p;
 }
 
 rb_node * rb_new(char * string)
@@ -45,6 +101,7 @@ rb_node * rb_new(char * string)
 	rb_node * rb = malloc(sizeof(rb_node));
 	rb->left = NULL;
 	rb->right = NULL;
+	rb->parent = NULL;
 	rb->string = string;
 	rb->rb = 1;
 	return rb;
--- src/redblack.h
+++ src/redblack.h
@@ -28,12 +28,15 @@ struct rb_node
 	char * string;
 	rb_node * left;
 	rb_node * right;
+	rb_node * parent;
 	//1 if red, 0 if black
 	int rb;
 };
 
 rb_node * rb_node_init();
 void rb_insert(rb_node * root, char * string);
+void rb_rotate_right(rb_node * node);
+void rb_rotate_left(rb_node * node);
 rb_node * rb_new(char * string);
 void rb_delete(rb_node * root, char * string);
 
